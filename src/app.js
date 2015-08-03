e56dac3212a3fc8c49b8f3419b3d38cb7ee984d7
Glenn Gonda <glenngonda@gmail.com>
2015-08-03 16:47:08 -0700

    air horn plays on click event

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 $(document).ready(function() {
 
-$('button').on('click', function() {
-  console.log('hi')
-})
+
+  $('#airHorn').click(function() {
+
+    $("<audio></audio>").attr({ 
+      'src':'audio/airHorn_1.mp3', 
+      'autoplay':'autoplay'
+    })
+
+  })
 
 });
-
