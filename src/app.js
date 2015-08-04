bbb2e1311b234b6ba14945e9ad15a016591af0ce
Glenn Gonda <glenngonda@gmail.com>
2015-08-03 17:18:25 -0700

    import sound effects. functionality of first row complete

--- src/app.js
+++ src/app.js
@@ -2,12 +2,31 @@ $(document).ready(function() {
 
 
   $('#airHorn').click(function() {
-
     $("<audio></audio>").attr({ 
       'src':'audio/airHorn_1.mp3', 
       'autoplay':'autoplay'
     })
+  })
 
+  $('#siren').click(function() {
+    $("<audio></audio>").attr({ 
+      'src':'audio/siren.mp3', 
+      'autoplay':'autoplay'
+    })
+  })
+
+  $('#explosion').click(function() {
+    $("<audio></audio>").attr({ 
+      'src':'audio/explosion.mp3', 
+      'autoplay':'autoplay'
+    })
+  })
+
+  $('#needle').click(function() {
+    $("<audio></audio>").attr({ 
+      'src':'audio/needle.mp3', 
+      'autoplay':'autoplay'
+    })
   })
 
 });
