8810ebcc23a0b7daf83f8599e8195ae8b4c8e305
Glenn Gonda <glenngonda@gmail.com>
2015-08-03 19:26:53 -0700

    3 of 4 rows fully functional with audio. also added record, stop, and play buttons with animation

--- src/app.js
+++ src/app.js
@@ -22,9 +22,65 @@ $(document).ready(function() {
     })
   })
 
-  $('#needle').click(function() {
+  $('#tuba').click(function() {
     $("<audio></audio>").attr({ 
-      'src':'audio/needle.mp3', 
+      'src':'audio/tuba.mp3', 
+      'autoplay':'autoplay'
+    })
+  })
+
+  $('#introducing').click(function() {
+    $("<audio></audio>").attr({ 
+      'src':'audio/introducing.mp3', 
+      'autoplay':'autoplay'
+    })
+  })
+
+  $('#letgo').click(function() {
+    $("<audio></audio>").attr({ 
+      'src':'audio/letgo.mp3', 
+      'autoplay':'autoplay'
+    })
+  })
+
+  $('#building').click(function() {
+    $("<audio></audio>").attr({ 
+      'src':'audio/building.mp3', 
+      'autoplay':'autoplay'
+    })
+  })
+
+  $('#busy').click(function() {
+    $("<audio></audio>").attr({ 
+      'src':'audio/busy.mp3', 
+      'autoplay':'autoplay'
+    })
+  })
+
+  $('#jayz').click(function() {
+    $("<audio></audio>").attr({ 
+      'src':'audio/jayz.mp3', 
+      'autoplay':'autoplay'
+    })
+  })
+
+  $('#getdown').click(function() {
+    $("<audio></audio>").attr({ 
+      'src':'audio/getdown.mp3', 
+      'autoplay':'autoplay'
+    })
+  })
+
+  $('#phaser').click(function() {
+    $("<audio></audio>").attr({ 
+      'src':'audio/phaser.mp3', 
+      'autoplay':'autoplay'
+    })
+  })
+
+  $('#luger').click(function() {
+    $("<audio></audio>").attr({ 
+      'src':'audio/luger.mp3', 
       'autoplay':'autoplay'
     })
   })
